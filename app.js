d18941a4f90ea95729b4844b11dfbbefeaeb78f9
Charlie Wu <charliewu@Charlies-MBP.home>
2017-06-04 17:29:40 -0400

    Basic multiplayer movement functionality

--- app.js
+++ app.js
@@ -13,178 +13,105 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(bodyParser.urlencoded({ extended: true })); 
-app.set('view engine', 'pug')
+//app.set('view engine', 'pug')
 
 app.use(express.static('./views'));
 app.use(cookieParser());
 
 var Datastore = require('nedb');
-db = new Datastore({ filename: 'database/users', autoload: true });
-activegames = new Datastore({ filename: 'database/activegames', autoload: true });
-gamereqs = new Datastore({ filename: 'database/gamereqs', autoload: true });
+gamedb = new Datastore({ filename: 'database/gamedb', autoload: true });
+
+//var activeusers = [][];
+var activeusersnum= 0;
 
 var users, hash;
 
-app.post('/confirmlogin', function(req, res){
-  if(req.body.username!=""&&req.body.username!=null){
-    hash = SHA512(req.body.password);
-    db.findOne({ username: req.body.username, password: hash }, function (err, docs) {
-      if(JSON.stringify(docs)!='null'){
-        console.log("success!");
-        res.cookie('username', req.body.username, { expires: 0, httpOnly: true });
-        res.cookie('password', hash, { expires: 0, httpOnly: true });
-        return res.render('index', {username: req.body.username});
-      }else{
-        res.render('login', {error: 'true', errormessage:'Invalid username or password, please try again'});
-      }
-    })
-  }else{
-    res.render('login', {error: 'true', errormessage:'Username cannot be blank.'});
-  }
-})
 
-app.post('/confirmcreate', function(req, res){
-  if(req.body.username!=""&&req.body.username!=null){
-    db.findOne({ username: req.body.username }, function (err, docs) {
-      if(JSON.stringify(docs)=='null'){
-        hash = SHA512(req.body.password);
-        users = {username:""+req.body.username, password:""+hash, name:""+req.body.name};
-        db.insert(users, function (err, newDoc){
-          if(!err){
-            res.render('login', {error: 'true', errormessage: 'Please login with your new credentials.'});
-            console.log("success");
-          }else{
-            res.render('createaccount', {error: 'true', errormessage:'Unexplained error, contact webmaster for assistance.'});
-            console.log("unexplained create error:   "+err);
-          }
-        })
-      }else{
-        res.render('createaccount', {error: 'true', errormessage:'Username already exists. Please try again.'});
-        console.log("req username exists");
-      }
-    })
-  }else{
-    res.render('createaccount', {error: 'true', errormessage:'Username cannot be blank.'});
-    console.log("username is blank");
-  }
-})
+var setup = false;
 
-/*
-app.get('/logout', function (req, res) {  
-  res.cookie('username', "loggedout", { expires: 0, httpOnly: true });
-  res.cookie('password', "loggedout", { expires: 0, httpOnly: true });
-  return res.render(__dirname+'/aawfiles/login', {error: 'false'});
-})
-*/
+if(setup==false){
+  console.log('setting up server options...');
+  var starttime=
+  setup=true;
+}
+
+
+
 http.listen((process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080), (process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'), function () {
-  console.log('Pool Tourney starting on port 8080.');
-  console.log('(c) 2017 RolzPro.com and Charlie Wu');
+  console.log('ez!BANG! minigame starting on port 8080.');
+  console.log('(c) 2017 Charlie Wu');
   console.log('Apache 2.0 License, github.com/byteplanes');
 })
 
-function getDateTime() {
-    var date = new Date();
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-    return month + "/" + day + "   " + hour + ":" + min ;
-}
-
-/* BEGIN FILE REDIRECTS*/
-/*
 app.get('/', function (req, res) {
-  if(req.cookies.username!="loggedout"&&req.cookies.username!=null){fs.exists("./aawdb/users/"+req.cookies.username+".txt", function(exists){if(exists){fs.readFile("./aawdb/users/"+req.cookies.username+".txt", function read(err, data){if(!err){if(req.cookies.password==data){
-    return res.render(__dirname+"/aawfiles/aawindex", {username: req.cookies.username, t1: "", b1: "", t2: "", b2: "", t3:"", b3:"", t4:"", b4:"", t5:"", b5:"", t6:"", b6:""});
-  }else{return res.render(__dirname+"/aawfiles/login", {error: 'true', errormessage:'Wrong Password. Try Again.'});}}else{return res.render(__dirname+"/aawfiles/login", {error: 'true', errormessage:'Unexplained error, contact webmaster for assistance.'});}})}else{return res.render(__dirname+"/aawfiles/login", {error: 'true', errormessage:'Username does not exist, to create account, see below.'});}})}else{return res.render(__dirname+"/aawfiles/login", {error:'false'});}
+  res.render('index');
 })
-*/
-
-app.get('/', function (req, res) {
-  var userc = req.cookies.username;
-  var passc = req.cookies.password;
-  if(userc==null||userc==""||userc=="loggedout")
-  {
-    res.render('login');
-  }else{
-    db.findOne({ username: userc, password: passc }, function (err, docs) {
-      if(JSON.stringify(docs)!='null'){
-        console.log("success!");
-        res.render('index', {error:'false', username: req.cookies.username});
-      }else{
-        res.render('login', {error: 'true', errormessage:'Invalid username or password, please try again'});
-      }
-    })
-  }
-  io.on('connection', function(socket){
-      socket.emit('setuser', userc);
-  });
-})
-
-app.get('/createaccount', function (req, res) {
-  return res.render('createaccount');
-})
-app.get('/login', function (req, res) {
-  return res.render('login');
-})
-/* END FILE REDIRECTS*/
 
 io.on('connection', function(socket){
-  socket.on('connected', function(msg){
+  socket.on('clientconnect', function(msg){
     console.log('user connected: ' + msg);
+    socket.emit('clientinfo', activeusersnum);
   });
-
-  socket.on('requpdate', function(data){
-    console.log(p1+" and "+p2+" have started a new game");
-    activegames.find({p1:p1}, function (err, docs){
-      console.log(JSON.stringify(docs));
-      socket.emit('updategame', p1+"||"+p2);
-    });
-  });
-
   socket.on('newgame', function(msg){
-    console.log("rolz   "+msg);
-    var newgamedata = {user: msg, time: getDateTime(), true: 'true'};
-    gamereqs.find({ true: 'true'}, function (err, docs) {
-      console.log(JSON.stringify(docs));
-      if(JSON.stringify(docs).length>10)
-      {
-        console.log("pair found yay!");
-        var p1 = JSON.stringify(docs).substring(10).substring(0, JSON.stringify(docs).search('"'));
-        var p2 = msg;
-        console.log("challengers: "+p1+", "+p2);
-        gamereqs.remove({ true: 'true' }, { multi: true }, function (err, numRemoved) {
-          console.log(numRemoved+" players removed from queue into battle");
-        });
-        var startgamedata = {p1: p1, p2: p2, p1avatar: "default", p2avatar: "default", p1stats: "1000 TROPHIES", p2stats: "1500 TROPHIES", time: getDateTime(), true: true};
-        activegames.insert(startgamedata, function (err, newDoc){
+    console.log('new game: ' + msg[1]);
+    activeusersnum++;
+    
+    gamedb.findOne({ username: msg[1] }, function (err, docs) {
+      if(JSON.stringify(docs)=='null'&&msg[1]!=''){
+        var newgamedata = {username:msg[1], xpos:50, ypos:50, color:'ff5900', lastx:1, lasty:1};
+        gamedb.insert(newgamedata, function (err, newDoc){
           if(!err){
-            console.log(p1+" and "+p2+" have started a new game");
-            activegames.find({p1:p1}, function (err, docs){// find one???
-              console.log(JSON.stringify(docs));
-              //var docsarr = docs.substring(3, docs.length-3).split("":"');
-              var curtime = new Date();
-              io.emit('startgame', p1+"||"+p2+"||"+"kaori"+"||"+"therock"+"||"+"aim"+"||"+(new Date(curtime.getFullYear,)));
-            });
+            gamedb.find({username:msg[1]}, function (err, doc){
+              if(!err){
+                socket.emit('startgame', doc);
+                console.log(doc[0]['username'])
+              }
+            })
           }
         })
       }else{
-        gamereqs.insert(newgamedata, function (err, newDoc){
-          if(!err){
-            console.log(msg+" is in the game queue");
-          }
-        })
+        socket.emit('uhoh', 'Please choose another username, this one is in use.');
       }
     })
   });
-});
-/*io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('requpdate', function(msg){
+    gamedb.find({ username: msg[1] }, function (err, docs) {
+      if(JSON.stringify(docs)!='null'&&msg[1]!=''){
+        var xchange=0;
+        var ychange=0;
+        console.log('update requested: ' + msg[1]+"   "+msg[0]);
+        if(msg[0]=="N"){
+          ychange=1;
+        }else if(msg[0]=="E"){
+          xchange=1;
+        }else if(msg[0]=="S"){
+          ychange=-1;
+        }else if(msg[0]=="W"){
+          xchange=-1;
+        }else{
+        }
+        if((docs[0]['xpos']>=49&&xchange>0)||(docs[0]['xpos']<=1&&xchange<0))
+        {
+          xchange=0;
+        }
+        if((docs[0]['ypos']>=49&&ychange>0)||(docs[0]['ypos']<=1&&ychange<0))
+        {
+          ychange=0;
+        }
+        var newgamedata = {username:docs[0]["username"], xpos:docs[0]['xpos']+xchange, ypos:docs[0]['ypos']+ychange, color:'ff5900', lastx:docs[0]['xpos'], lasty:docs[0]['ypos']};
+        gamedb.update({username: msg[1]}, newgamedata, {}, function (err, newDoc){
+          if(!err){
+            gamedb.find({username:msg[1]}, function (err, doc){
+              if(!err){
+                io.emit('update', doc);
+              }
+            })
+          }
+        })
+      }else{
+        socket.emit('uhoh', 'SOMETHING WENT WRONG WITH YOUR REQUEST YOUR USERNAME IS MESSED UP SRY');
+      }
+    })
   });
-});*/
+
+});
