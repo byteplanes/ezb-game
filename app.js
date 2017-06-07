33040fe0918e1ac9609e3f32ff448167736d5ae4
Charlie Wu <charliewu@Charlies-MBP.home>
2017-06-06 21:36:48 -0400

    ALL MOVEMENTS AND MISSILES HANDED SERVERSIDE

--- app.js
+++ app.js
@@ -13,14 +13,9 @@ app.use(express.static('./views'));
 
 var Datastore = require('nedb');
 gamedb = new Datastore({ filename: 'database/gamedb', autoload: true });
-missiledb = new Datastore({ filename: 'database/missiledb', autoload: true });
 gamedb.persistence.setAutocompactionInterval(10000);
-missiledb.persistence.setAutocompactionInterval(10000);
-//var activeusers = [][];
 var activeusersnum= 0;
-
-var users, hash;
-
+var game = setInterval(function(){rungame()}, 50);
 
 var setup = false;
 
@@ -48,19 +43,28 @@ io.on('connection', function(socket){
     console.log('user connected: ' + msg);
     socket.emit('clientinfo', activeusersnum);
   });
+  socket.on('quit', function(msg){
+    gamedb.remove({username:msg}, function(err, numRemoved){
+      if(!err){
+        console.log(msg+" successfully removed! "+numRemoved);
+        activeusersnum--;
+      }
+    });
+  });
   socket.on('newgame', function(msg){
     console.log('new game: ' + msg[1]);
     activeusersnum++;
-    
     gamedb.findOne({ username: msg[1] }, function (err, docs) {
       if(JSON.stringify(docs)=='null'&&msg[1]!=''){
-        var newgamedata = {username:msg[1], xpos:50, ypos:50, color:'ff5900', lastx:1, lasty:1, missiles:0};
+        var newgamedata = {username:msg[1], xpos:50, ypos:50, color:'ff5900', active:true, direction:"N", type:"player"};
         gamedb.insert(newgamedata, function (err, newDoc){
           if(!err){
-            gamedb.find({username:msg[1]}, function (err, doc){
+            var newmissilegamedata = {username:msg[1]+"_MISSILE", xpos:50, ypos:50, color:'67ff4c', active:false, direction:"N", type:"missile"};
+            gamedb.insert(newmissilegamedata, function (err, newDoc){
               if(!err){
-                socket.emit('startgame', doc);
-                console.log(doc[0]['username'])
+                gamedb.find({username:msg[1]}, function(err, doc){
+                  socket.emit('startgame', doc);
+                });
               }
             })
           }
@@ -71,130 +75,19 @@ io.on('connection', function(socket){
     })
   });
   socket.on('requpdate', function(msg){
-    gamedb.find({ username: msg[1] }, function (err, docs) {
+    gamedb.findOne({ username: msg[1] }, function (err, docs) {
       if(JSON.stringify(docs)!='null'&&msg[1]!=''){
-        var xchange=0;
-        var ychange=0;
-        var ismissile=false;
-        var mchange = 0;
-        if((msg[0]=="UP"||msg[0]=="LEFT"||msg[0]=="RIGHT"||msg[0]=="DOWN")&&docs[0]['missiles']==0)
-        {
-          //msg[1]=msg[1]+"MISSLE";
-          mchange=1;
-          console.log(msg[1]);
-          var newmissilegamedata = {username:msg[1], xpos:docs[0]['xpos'], ypos:docs[0]['ypos'], color:'62d642', lastx:1, lasty:1, missiles:-1, direction:msg[0]};
-          missiledb.insert(newmissilegamedata, function (err, newDoc){
+        if(msg[0]=="UP"||msg[0]=="DOWN"||msg[0]=="LEFT"||msg[0]=="RIGHT"){
+          docs['username']=docs['username']+"_MISSILE";
+          docs['color']='67ff4c';
+          docs['type']="missile";
+        }
+        var newgamedata = {username:docs["username"], xpos:docs['xpos'], ypos:docs['ypos'], color:docs['color'], active:true, direction:msg[0], type:docs['type']};
+          gamedb.update({username: docs["username"]}, newgamedata, {}, function (err, newDoc){
             if(!err){
-              missiledb.find({username:msg[1]}, function (err, mdoc){
-                if(!err){
-                  var newgamedata = {username:docs[0]["username"], xpos:docs[0]['xpos']+xchange, ypos:docs[0]['ypos']+ychange, color:'62d642', lastx:docs[0]['xpos'], lasty:docs[0]['ypos'], missiles:docs[0]['missiles']+mchange};
-                  gamedb.update({username: msg[1]}, newgamedata, {}, function (err, newDoc){
-                    if(!err){
-                      gamedb.find({username:msg[1]}, function (err, doc){
-                        if(!err){
-                          doc[0]['username']=doc[0]['username']+"MISSILE";
-                          io.emit('update', doc);
-                        }
-                      })
-                    }
-                  })
-                }else{
-                  console.log("darn inner error")
-                }
-              })
-            }else{console.log('oops err')}
-          })
-        }
-        var mxchange=0;
-        var mychange=0;
-        if((new Date).getTime()-50>time)
-        {
-          time=(new Date).getTime();
-          missiledb.find({username:msg[1]}, function (err, docs){
-            if(JSON.stringify(docs).length>5){
-              if(!err){
-              if(docs[0]['direction']=="UP"){
-                mychange=-1;
-              }else if(docs[0]['direction']=="LEFT")
-              {
-                mxchange=-1;
-              }else if(docs[0]['direction']=="RIGHT")
-              {
-                mxchange=1;
-              }else if(docs[0]['direction']=="DOWN")
-              {            
-                mychange=1;
-              }
-              var newgamedata = {username:docs[0]["username"], xpos:docs[0]['xpos']+mxchange, ypos:docs[0]['ypos']+mychange, color:'62d642', lastx:docs[0]['xpos'], lasty:docs[0]['ypos'], missiles:docs[0]['missiles'], direction:docs[0]['direction']};
-              missiledb.update({username: msg[1]}, newgamedata, {}, function (err, newDoc){
-                if(!err){
-                  missiledb.find({username:msg[1]}, function (err, doc){
-                    if(!err){
-                      if(doc[0]['xpos']>50||doc[0]['xpos']<0||doc[0]['ypos']>50||doc[0]['ypos']<0)
-                      {
-                        missiledb.remove({username:doc[0]['username']}, function (err, numRemoved){
-                          gamedb.update({username: msg[1]}, { $set: { missiles: 0 } }, {}, function (err, newDoc){
-                            if(!err){
-                              console.log('cleared missile');
-                            }
-                          })
-                        });
-                      }else{
-                        doc[0]['username']=doc[0]['username']+"MISSILE";
-                        io.emit('update', doc);
-                      }
-                    }
-                  })
-                }
-              })
-            }else{
-              console.log("darn inner error")
-            }
+              //updated
             }
           })
-        }
-        //console.log('update requested: ' + msg[1]+"   "+msg[0]);
-        if(msg[0]=="N"){
-          ychange=1;
-        }else if(msg[0]=="E"){
-          xchange=1;
-        }else if(msg[0]=="S"){
-          ychange=-1;
-        }else if(msg[0]=="W"){
-          xchange=-1;
-        }else{
-        }
-        if((docs[0]['xpos']>=50&&xchange>0)||(docs[0]['xpos']<=0&&xchange<0))
-        {
-          xchange=0;
-        }
-        if((docs[0]['ypos']>=50&&ychange>0)||(docs[0]['ypos']<=0&&ychange<0))
-        {
-          ychange=0;
-        }
-        var newgamedata = {username:docs[0]["username"], xpos:docs[0]['xpos']+xchange, ypos:docs[0]['ypos']+ychange, color:'ff5900', lastx:docs[0]['xpos'], lasty:docs[0]['ypos'], missiles:docs[0]['missiles']+mchange};
-        gamedb.update({username: msg[1]}, newgamedata, {}, function (err, newDoc){
-          if(!err){
-            gamedb.find({username:msg[1]}, function (err, doc){
-              if(!err){
-                io.emit('update', doc);
-              }
-            })
-          }
-        })
-        missiledb.find({username:docs[0]['username']}, function (err, mdoc){
-          if(!err){
-            if(mdoc[0]!=undefined){
-              //console.log(mdoc[0]);
-              gamedb.find({xpos:mdoc[0]['xpos'], ypos:mdoc[0]['ypos']}, function (err, gdoc){
-                if(!err&&gdoc[0]!=undefined&&(gdoc[0]['username']!=mdoc[0]['username'])){
-                    console.log('COLLISION: '+gdoc[0]['username']+" and "+mdoc[0]['username']);
-                    io.emit('uhoh',gdoc[0]['username']+" WAS KILLED BY "+mdoc[0]['username'])
-                }
-              });
-            }
-          }
-        });
       }else{
         socket.emit('uhoh', 'SOMETHING WENT WRONG WITH YOUR REQUEST YOUR USERNAME IS MESSED UP SRY');
       }
@@ -202,3 +95,67 @@ io.on('connection', function(socket){
   });
 
 });
+
+function rungame(){
+  //console.log((new Date).getTime());
+  gamedb.find({active:true}, function(err, alldocs){
+    //console.log(alldocs.length);
+    for(var i = 0; i < alldocs.length; i++){
+      updateall(i, alldocs);
+    }
+  });
+}
+
+function updateall(i, alldocs){
+  var xchange = 0;
+  var ychange = 0;
+  if(alldocs[i]['direction']=="N"||alldocs[i]['direction']=="UP"){
+    ychange=1;
+  }else if(alldocs[i]['direction']=="E"||alldocs[i]['direction']=="RIGHT"){
+    xchange=1;
+  }else if(alldocs[i]['direction']=="S"||alldocs[i]['direction']=="DOWN"){
+    ychange=-1;
+  }else if(alldocs[i]['direction']=="W"||alldocs[i]['direction']=="LEFT"){
+    xchange=-1;
+  }else{
+  }
+  if((alldocs[i]['xpos']>=50&&xchange>0)||(alldocs[i]['xpos']<=0&&xchange<0))
+  {
+    xchange=0;
+    if(alldocs[i]['type']=="missile"){
+      alldocs[i]['active']=false;
+    }
+  }
+  if((alldocs[i]['ypos']>=50&&ychange>0)||(alldocs[i]['ypos']<=0&&ychange<0))
+  {
+    ychange=0;
+    if(alldocs[i]['type']=="missile"){
+      alldocs[i]['active']=false;
+    }
+  }
+  if(alldocs[i]['type']=="missile"){
+    gamedb.find({active:true, type:"player"}, function(err, players){
+      for(var j = 0; j < players.length; j++){
+        checkcollide(alldocs[i], players[j]);
+      }
+    });
+  }
+  var newgamedata = {username:alldocs[i]["username"], xpos:alldocs[i]['xpos']+xchange, ypos:alldocs[i]['ypos']+ychange, color:alldocs[i]['color'], active:alldocs[i]['active'], direction:alldocs[i]['direction'], type:alldocs[i]['type']};
+  gamedb.update({username: alldocs[i]['username']}, newgamedata, {}, function (err, newDoc){
+    if(!err&&i==alldocs.length-1){
+      gamedb.find({active:true}, function (err, doc){
+        if(!err){
+          io.emit('update', doc);
+        }
+      })
+    }
+  })
+}
+
+function checkcollide(obj1, obj2){
+  if(Math.abs(obj1['xpos']-obj2['xpos'])<=4&&Math.abs(obj1['ypos']-obj2['ypos'])<=4&&obj1['username'].substring(0,obj1['username'].length-8)!=obj2['username']){
+    io.emit('uhoh', obj1['username'].substring(0,obj1['username'].length-8)+" KILLED "+obj2['username']+"!!!")
+  }else{
+    //no collision
+  }
+}
